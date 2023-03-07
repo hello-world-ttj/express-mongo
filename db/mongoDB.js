22c77a3e2ce26989ff4d79ecc921cba092242cb5
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2023-03-07 20:20:43 +0530

    mongodb connect change

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -3,8 +3,18 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 
-mongoose.connect(mongoString).then(() => {
-    console.log("Database connection established...!");
-}).catch((err) => {
-    console.log("Database connection error: "+err);
-})
+// mongoose.connect(mongoString).then(() => {
+//     console.log("Database connection established...!");
+// }).catch((err) => {
+//     console.log("Database connection error: "+err);
+// })
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(mongoString);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
